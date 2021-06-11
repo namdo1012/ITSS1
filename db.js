6523839701b5887d3f5840d9fd91b0daf932f7d1
ngocmunghy <mung09021999@gmail.com>
2021-06-11 19:30:50 +0700

    add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+// import { LowSync, JSONFileSync } from 'lowdb'
+// const db = new LowSync(new JSONFileSync('db.json'));
+
+// db.read();
+
+// if(!db.data) {
+// 	db.data = {users: []}
+// 	db.write();
+// }
+
+// export default db;
