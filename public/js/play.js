a3ea0e8938622e28a14bbd1684606d46723b065f
namdo1012 <namdo10121999@gmail.com>
2021-06-13 17:01:10 +0700

    Feature: Save score

--- public/js/play.js
+++ public/js/play.js
@@ -1,5 +1,11 @@
 const handleSaveScore = () => {
-	const score = document.getElementById("score").innerHTML;
-  axios.post("/save-game", { score });
+  const score = document.getElementById("score").innerHTML;
+  const savedName = document.getElementById("saved-name").value;
+  axios.post("/save-game", { score, savedName });
   window.location.href = "/";
+};
+
+const handleShowModal = () => {
+  const score = document.getElementById("score").innerHTML;
+  document.getElementById("score1").value = score;
 };
--- public/js/save.js
+++
@@ -1,5 +0,0 @@
-let x = document.getElementById('score').innerHTML;
-
-document.getElementById('score1').value = x;
-
-console.log(x);
